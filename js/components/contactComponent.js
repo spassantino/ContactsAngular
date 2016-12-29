40564a47e166ee014d8ceb6c423509d2a8dc8e40
Sarah Passantino <spassantino@yahoo.com>
2016-12-29 14:30:02 -0700

    non working dropdown added

--- js/components/contactComponent.js
+++ js/components/contactComponent.js
@@ -14,13 +14,16 @@ template :
           <h1>
             ngContacts
           </h1>
-          <input type="text" placeholder="First Name" ng-model="$ctrl.newContact" />
-          <input type="text" placeholder="Last Name" ng-model="$ctrl.newContact" />
-          <input type="text" placeholder= "Email" ng-model="$ctrl.newContact" />
-          <input type="text" placeholder= "Phone" ng-model="$ctrl.newContact" />
-          <input type="text" placeholder= "Street" ng-model="$ctrl.newContact" />
-          <input type="text" placeholder= "City" ng-model="$ctrl.newContact" />
-          <input type="text" placeholder= "Zip" ng-model="$ctrl.newContact" />
+          <input type="text" placeholder="First Name" ng-model="$ctrl.newContact" /><br>
+          <input type="text" placeholder="Last Name" ng-model="$ctrl.newContact" /><br>
+          <input type="text" placeholder= "Email" ng-model="$ctrl.newContact" /><br>
+          <input type="text" placeholder= "Phone" ng-model="$ctrl.newContact" /><br>
+          <input type="text" placeholder= "Street" ng-model="$ctrl.newContact" /><br>
+          <input type="text" placeholder= "City" ng-model="$ctrl.newContact" /><br>
+          <select ng-model="states" ng-options="value.abbreviation for value in states">
+    <option>--</option>
+</select><br>
+          <input type="text" placeholder= "Zip" ng-model="$ctrl.newContact" /><br>
           <button ng-click="$ctrl.addContact($ctrl.newContact);">Create</button>
           <contact-list data="$ctrl.contacts" show-complete="$ctrl.showComplete"></contact-list>
           `
